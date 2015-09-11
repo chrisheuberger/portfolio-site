ed973cb1e0c20e9adbacc70bfb32de455f3a472e
Chris Heuberger <chrisheuberg@gmail.com>
2015-09-11 18:44:14 -0400

    spacing fixes

--- gtrainlovestory/js/scripts.js
+++ gtrainlovestory/js/scripts.js
@@ -88,14 +88,16 @@ $(function() {
 
 	$(function() {
 
+		var headerHeight = $("header").height() - 60;
+
 		$('a[href*=#]:not([href=#])').click(function() {
 			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top
-					}, 2000);
+						scrollTop: target.offset().top - headerHeight
+					}, 1500);
 					return false;
 				}
 			}
